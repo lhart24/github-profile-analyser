5960e7c8cc8b88d4492678bbe53a519af68bd024
lhart24 <lhartley208@gmail.com>
2026-03-14 00:51:04 +1000

    Create server.js

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express")
+
+const app = express()
+
+app.use(express, json())
+app.use("/api/github", githubRoutes)
+
+const PORT = process.env.PORT || 5001
+
+app.listen(PORT, () =>{
+    console.log(`Server running on ${PORT}`)
+})
