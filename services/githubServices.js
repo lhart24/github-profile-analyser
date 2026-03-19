2a05003eee250c95c3b183909aaff4ade3d34aa3
lhart24 <lhartley208@gmail.com>
2026-03-20 00:09:24 +1000

    comments

--- services/githubServices.js
+++ services/githubServices.js
@@ -1,5 +1,6 @@
 const axios = require("axios")
 
+// link to get data from github api
 exports.getRepos = async (username) => {
     const response = await axios.get(
         `https://api.github.com/users/${username}/repos`
