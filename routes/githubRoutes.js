72f6ecf0122ab1a4d4a906707cdc7cb87b39bc2a
lhart24 <lhartley208@gmail.com>
2026-03-20 00:10:04 +1000

    comment

--- routes/githubRoutes.js
+++ routes/githubRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router()
 
 const githubController = require("../controllers/githubController")
 
+// route for postman
 router.get("/analyser/:username", githubController.analyseUser)
 
 module.exports = router
