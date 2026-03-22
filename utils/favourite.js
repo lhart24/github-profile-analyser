769b57338293b7eb2c1221784cc11a9dcebc5e37
lhart24 <lhartley208@gmail.com>
2026-03-23 00:21:56 +1000

    Update favourite.js

--- utils/favourite.js
+++ utils/favourite.js
@@ -5,4 +5,7 @@ const axios = require("axios")
 
 async function getFavouriteLanguage(username) {
     const repos = await axios.get(`https://api.github.com/users/${username}/repos`)
+
+    // use total bit count for each language to find favourite language
+
 }
